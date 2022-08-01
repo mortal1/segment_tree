da85817295b814bf36246ac7c552644e920d3cc3
Lake <laeequejamdar@gmail.com>
2022-08-02 01:19:24 +1000

    added fix for set-sum tree: in updates it used to set the segment query value to the query of the range that we are updating over

--- segment-tree/misc_segment_trees.py
+++ segment-tree/misc_segment_trees.py
@@ -1,5 +1,5 @@
 from copy import copy, deepcopy
-from math import log2
+from math import log2, floor
 
 # incomplete
 class StrictBottomUpSetSumSegmentTree():
@@ -42,7 +42,7 @@ class StrictBottomUpSetSumSegmentTree():
 class SetSumSegmentTree():
     
     def __init__(self, n):
-        self.n = 2**log2(n)
+        self.n = int(2**(floor(log2(n-1))+1))
         self.qarr = [0]*(2*self.n)
         self.uarr = [None]*(2*self.n)
 
@@ -59,30 +59,36 @@ class SetSumSegmentTree():
         self.uarr[i] = None
 
     def update(self, start, end, value):
-        return self._update(self, start, end, value, 1, 0, self.n)
+        return self._update(start, end, value, 1, 0, self.n)
 
     def query(self, start, end):
-        return self._update(self, start, end, None, 1, 0, self.n)
+        return self._update(start, end, None, 1, 0, self.n)
 
     def _update(self, start, end, value, i, istart, iend):
+        self.push(i, istart, iend)
+
         if end <= istart or start >= iend:
-            return 0
+            if value is not None:
+                return self.qarr[i]
+            else:
+                return 0
 
         elif start <= istart and end >= iend:
             if value is not None:
                 self.uarr[i] = value
             self.push(i, istart, iend)
 
+            # print(locals(), self.__dict__)
             return self.qarr[i]
 
         else:
-            self.push(i, istart, iend)
-
             mid = (istart + iend) // 2
-            left = self._update(self, start, end, value, 2*i, istart, mid)
-            right = self._update(self, start, end, value, 2*i+1, mid, iend)
-            self.qarr[i] = left + right
+            left = self._update(start, end, value, 2*i, istart, mid)
+            right = self._update(start, end, value, 2*i+1, mid, iend)
+            if value is not None:
+                self.qarr[i] = left + right
 
+            # print(locals(), self.__dict__)
             return self.qarr[i]
 
 
@@ -106,14 +112,14 @@ class GenericSetSumSegmentTree():
 
     u_id = None
     q_id = 0
-    
+
     def __init__(self, n):
-        self.n = 2**log2(n)
+        self.n = int(2**(floor(log2(n-1))+1))
         self.qarr = [0]*(2*self.n)
         self.uarr = [None]*(2*self.n)
 
-    def mid(start, end):
-        return (start + end) / 2
+    # def mid(start, end):
+    #     return (start + end) / 2
 
     def merge_uu(a, b):
         if a is not None:
@@ -130,6 +136,18 @@ class GenericSetSumSegmentTree():
         else:
             return q
 
+    def push(self, i, istart, iend):
+        if self.uarr[i] == self.u_id:
+            return
+        
+        self.qarr[i] = self.merge_uq(self.uarr[i], self.qarr[i], istart, iend)
+
+        if i < self.n:
+            self.uarr[i*2] = self.merge_uu(self.uarr[i], self.uarr[i*2])
+            self.uarr[i*2+1] = self.merge_uu(self.uarr[i], self.uarr[i*2+1])
+
+        self.uarr[i] = self.u_id
+
     def update(self, start, end, value):
         return self._update(self, start, end, value, None, 1, 0, self.n)
 
@@ -138,39 +156,48 @@ class GenericSetSumSegmentTree():
 
 
     def _update(self, start, end, value, i, istart, iend):
+        self.push(i, istart, iend)
         if end <= istart or start >= iend:
-            return 0
+            pass
 
         elif start <= istart and end >= iend:
-            if value is not None:
-                self.uarr[i] = value
+            self.update_vals[i] = self.merge_uu(value, self.update_vals[i])
             self.push(i, istart, iend)
 
-            return self.qarr[i]
-
-        else:
-            self.push(i, istart, iend)
-
-            mid = (istart + iend) // 2
-            left = self._update(self, start, end, value, 2*i, istart, mid)
-            right = self._update(self, start, end, value, 2*i+1, mid, iend)
-            self.qarr[i] = left + right
-
-            return self.qarr[i]
-
-    def _update(self, start, end, value, off_value, i, istart, iend):
-        if end <= istart or start >= iend:
-            return self.q_id
-
-        elif start <= istart and end >= iend:
-            self.update_vals[i] = value
             
         else:
             mid = self.mid(istart, iend)
             off_value = self.setval(off_value, self.uarr[i])
-            self._update(self, start, end, value, 2*i, istart, mid)
-            self._update(self, start, end, value, 2*i+1, mid, iend)
+            left = self._update(self, start, end, value, 2*i, istart, mid)
+            right = self._update(self, start, end, value, 2*i+1, mid, iend)
             self.query_vals[i] = \
                 self.merge_qq(
                     self.merge_,
                     )
+        
+        return self.query_vals[i]
+
+    # copied code
+#    def _update(self, start, end, value, i, istart, iend):
+#         if end <= istart or start >= iend:
+#             return 0
+
+#         elif start <= istart and end >= iend:
+#             if value is not None:
+#                 self.uarr[i] = value
+#             self.push(i, istart, iend)
+
+#             print(locals(), self.__dict__)
+#             return self.qarr[i]
+
+#         else:
+#             self.push(i, istart, iend)
+
+#             mid = (istart + iend) // 2
+#             left = self._update(start, end, value, 2*i, istart, mid)
+#             right = self._update(start, end, value, 2*i+1, mid, iend)
+#             if value is not None:
+#                 self.qarr[i] = left + right
+
+#             print(locals(), self.__dict__)
+#             return self.qarr[i]
